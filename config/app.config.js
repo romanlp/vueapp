a46a7679c0471171afe22deb75c14842d909fd4b
Roman Lapacherie <roman.lapacherie@gmail.com>
2018-07-14 19:10:16 +0100

    Delete app.config.js

--- config/app.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  mapboxToken:
-    "pk.eyJ1Ijoicm9tYW5scCIsImEiOiJjajE5M3N3aWMwMDRiMnduNTVzZGZvNTZpIn0.TzeuoHAD7BZgU-prduPkRw",
-  mapStyles: {
-    default: "mapbox://styles/mapbox/streets-v10",
-    satellite: "mapbox://styles/mapbox/satellite-v9"
-  }
-};
